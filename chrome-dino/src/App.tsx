b85b861251a53f558370bf3576f05b43d455e39d
DeekshithReddyA <deekshithreddy351@gmail.com>
2026-02-15 15:06:14 +0530

    .

--- chrome-dino/src/App.tsx
+++ chrome-dino/src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+     <div>
+      Home Page
+      </div> 
     </>
   )
 }
 
-export default App
+export default App;
