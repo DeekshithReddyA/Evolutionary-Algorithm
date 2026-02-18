0c0e5e6494f19b1e1d1931b54f9d6ac89a2ee9bc
DeekshithReddyA <deekshithreddy351@gmail.com>
2026-02-19 02:15:30 +0530

    made the lil guy jump. ufffff

--- chrome-dino/game.js
+++ chrome-dino/game.js
@@ -1,30 +1,103 @@
+var GROUND_Y = 300;
+var GROUND_X_START = 100;
+var GROUND_X_END = 800;
+var DINO_Y = 250;
+var JUMP_STRENGTH = -4.5;
 var Dino = /** @class */ (function () {
-    // have to add image to this constructor
     function Dino() {
         this.x = 105;
-        this.y = 250;
+        this.y = DINO_Y;
         this.width = 50;
         this.height = 50;
+        this.velocity = 0; // negative for upward direction, positive for downward direction
+        this.gravity = 0.1; // small intervals to show more re-renders leading to more fps, more fps = more smoothness
+        this.jumping = false;
         this.image = new Image();
         this.image.src = "/public/dino.png";
     }
     Dino.prototype.draw = function (ctx) {
-        var _this = this;
-        this.image.onload = function () {
-            ctx.drawImage(_this.image, _this.x, _this.y, _this.width, _this.height);
-        };
+        if (ctx) {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+    };
+    Dino.prototype.jump = function () {
+        if (!this.jumping) {
+            this.jumping = true;
+            this.velocity = JUMP_STRENGTH;
+        }
+        console.log(this.y);
+    };
+    Dino.prototype.update = function () {
+        if (!this.jumping)
+            return;
+        this.velocity += this.gravity;
+        this.y += this.velocity;
+        if (this.y >= GROUND_Y - this.height) {
+            this.y = DINO_Y;
+            this.jumping = false;
+        }
     };
     return Dino;
 }());
-var canvas = document.getElementById("myCanvas") || null;
-var ctx = canvas.getContext("2d");
-if (ctx) {
-    // ctx.lineWidth = 5;
-    ctx.beginPath();
-    ctx.moveTo(100, 300);
-    ctx.lineTo(800, 300);
-    // ctx.closePath();
-    ctx.stroke();
-    var dino1 = new Dino();
-    dino1.draw(ctx);
-}
+var Game = /** @class */ (function () {
+    function Game() {
+        this.canvas = document.getElementById("myCanvas");
+        this.ctx = this.canvas.getContext("2d");
+        this.dino = new Dino();
+        this.dino1 = new Dino();
+        this.playing = false;
+        console.log("constructor called");
+        this._waitForImageLoad();
+        this._bindInput();
+    }
+    Game.prototype.start = function () {
+        if (!this.playing) {
+            this.playing = true;
+        }
+    };
+    Game.prototype._loop = function () {
+        var _this = this;
+        var _a;
+        console.log("inside loop");
+        (_a = this.ctx) === null || _a === void 0 ? void 0 : _a.clearRect(0, 0, 800, 500);
+        this.drawGround();
+        this.dino.update();
+        this.dino.draw(this.ctx);
+        requestAnimationFrame(function () { return _this._loop(); });
+    };
+    Game.prototype.drawGround = function () {
+        if (this.ctx) {
+            // ctx.lineWidth = 5;
+            // ctx.beginPath();
+            this.ctx.moveTo(GROUND_X_START, GROUND_Y);
+            this.ctx.lineTo(GROUND_X_END, GROUND_Y);
+            this.ctx.stroke();
+        }
+    };
+    Game.prototype._bindInput = function () {
+        var _this = this;
+        window.addEventListener("keydown", function (e) {
+            if (e.code === "Space" || e.code === "ArrowUp") {
+                console.log("jump");
+                e.preventDefault();
+                _this.dino.jump();
+            }
+        });
+    };
+    Game.prototype._waitForImageLoad = function () {
+        var _this = this;
+        console.log("Waiting for image to load");
+        var loaded = 0;
+        var total = 2;
+        var onLoad = function () {
+            loaded++;
+            console.log("loaded: ", loaded);
+            if (loaded === total)
+                _this._loop();
+        };
+        this.dino.image.onload = onLoad;
+        this.dino1.image.onload = onLoad;
+    };
+    return Game;
+}());
+var game = new Game();
--- chrome-dino/game.ts
+++ chrome-dino/game.ts
@@ -1,41 +1,124 @@
+const GROUND_Y: number = 300;
+const GROUND_X_START: number = 100;
+const GROUND_X_END: number = 800;
+
+const DINO_Y = 250;
+
+const JUMP_STRENGTH = -4.5;
 
 class Dino{
     x: number = 105;
-    y: number = 250;
+    y: number = DINO_Y;
     width: number = 50;
     height: number = 50;
     image: HTMLImageElement;
+    velocity: number = 0; // negative for upward direction, positive for downward direction
+    gravity: number = 0.1; // small intervals to show more re-renders leading to more fps, more fps = more smoothness
+    jumping: boolean = false;
 
-    // have to add image to this constructor
     constructor(){
-
         this.image = new Image();
         this.image.src = "/public/dino.png";
     }
 
-    draw(ctx: CanvasRenderingContext2D){
-        this.image.onload = () => {
+    draw(ctx: CanvasRenderingContext2D | null){
+        if(ctx){
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+    }
+
+    
+    jump(){
+        if(!this.jumping){
+            this.jumping = true;
+            this.velocity = JUMP_STRENGTH;
+        }     
+        console.log(this.y);   
+    }
+
+    update(){
+        if(!this.jumping) return;
+        this.velocity += this.gravity;
+        this.y += this.velocity;
+
+        if(this.y >= GROUND_Y - this.height){
+            this.y = DINO_Y;
+            this.jumping = false;
         }
     }
 }
 
 
+class Game{    
+    canvas: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D | null;
+    dino: Dino;
+    playing: boolean;
+    dino1: Dino;
+    constructor(){
+        this.canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+        this.ctx = this.canvas.getContext("2d");
+        this.dino = new Dino();
+        this.dino1 = new Dino();
+        this.playing = false;
 
-const canvas: HTMLCanvasElement | null = document.getElementById("myCanvas") as HTMLCanvasElement || null;
-const ctx = canvas!.getContext("2d");
+        console.log("constructor called");
 
-if(ctx){
-    // ctx.lineWidth = 5;
+        this._waitForImageLoad();
+        this._bindInput();
+    }
 
-    ctx.beginPath();
-    ctx.moveTo(100,300);
-    ctx.lineTo(800, 300);
+    start(){
+        if(!this.playing){
+            this.playing = true;
+        }
+    }
+    
+    _loop(){
+        console.log("inside loop");
+        this.ctx?.clearRect(0,0,800,500);
+        this.drawGround();
+        this.dino.update();
+        this.dino.draw(this.ctx);
+        requestAnimationFrame(()=>this._loop());
+    }
+    
+    drawGround(){
+        if(this.ctx){
+            // ctx.lineWidth = 5;
+            
+            // ctx.beginPath();
+            this.ctx.moveTo(GROUND_X_START,GROUND_Y);
+            this.ctx.lineTo(GROUND_X_END, GROUND_Y);
+            this.ctx.stroke();            
+            
+        }
+    }
 
-    // ctx.closePath();
-    ctx.stroke();
 
-    const dino1 = new Dino();
+    _bindInput(){
+        window.addEventListener("keydown", (e) => {
+            if(e.code === "Space" || e.code === "ArrowUp"){
+                console.log("jump");
+                e.preventDefault();
+                this.dino.jump();
+            }
+        });
+    }
+    
+    _waitForImageLoad(){
+        console.log("Waiting for image to load");
+        let loaded = 0;
+        const total = 2;
+        const onLoad = () => {
+            loaded++;
+            console.log("loaded: ", loaded);
+            if(loaded === total) this._loop();
+        };
 
-    dino1.draw(ctx);
+        this.dino.image.onload = onLoad;
+        this.dino1.image.onload = onLoad;
+    }
 }
+
+const game = new Game();
